fedad332d20d6399ebf9cb98b145e6b089fb982d
adriandamiao <adriandami_@hotmail.com>
2023-03-25 13:53:35 -0300

    Finaliza jogo

--- Dots.cs
+++ Dots.cs
@@ -1,3 +1,5 @@
+using DotsGame.Extensions;
+
 namespace DotsGame;
 
 public class Dots
@@ -6,6 +8,8 @@ public class Dots
     private const int TamanhoDoTabuleiro = 5;
     
     private Tabuleiro tabuleiro = new();
+
+    private char[,] TabuleiroOficial = new char[5, 5];
     private No noRaiz = new No();
     private bool[] JogadasPossiveis { get; set; } = new bool[12]{
         true, true, true, true,
@@ -73,6 +77,10 @@ public class Dots
             PreencheArvoreDePossibilidades(noRaiz, JogadasPossiveis, jogador);
         }
 
+        AvaliaMiniMax(noRaiz, jogador);
+
+        Jogar(noRaiz, jogador);
+
         // Não finalizado ainda
         while(!noRaiz.JogadasFinalizadas())
         {
@@ -120,33 +128,33 @@ public class Dots
         System.Console.WriteLine($"Jogador {(pontuacaoJogador1 > pontuacaoJogador2 ? "1" : "2")} Venceu!\n");
     }
 
-    // private int EncontrarGanhador(No no)
-    // {
-    //     if(!no.JogadasFinalizadas()) {
-    //         return 2; // Jogo não finalizado ainda.
-    //     }
+    private int EncontrarGanhador(No no)
+    {
+        if(!no.JogadasFinalizadas()) {
+            return 2; // Jogo não finalizado ainda.
+        }
         
 
-    //     var locaisDePontuacao = new List<(int linha, int coluna)>(){
-    //         (1, 1), (1, 3), (3, 1), (3, 3),
-    //     };
+        var locaisDePontuacao = new List<(int linha, int coluna)>(){
+            (1, 1), (1, 3), (3, 1), (3, 3),
+        };
 
-    //     int pontuacaoJogador = 0;
-    //     int pontuacaoMaquina = 0;
+        int pontuacaoJogador = 0;
+        int pontuacaoMaquina = 0;
 
-    //     foreach(var local in locaisDePontuacao)
-    //     {
-    //         if(no.Tabuleiro[local.linha, local.coluna] == '1')
-    //             pontuacaoJogador++;
-    //         else
-    //             pontuacaoMaquina++;
-    //     }
+        foreach(var local in locaisDePontuacao)
+        {
+            if(no.Tabuleiro[local.linha, local.coluna] == '1')
+                pontuacaoJogador++;
+            else
+                pontuacaoMaquina++;
+        }
 
-    //     if(pontuacaoJogador == pontuacaoMaquina)
-    //         return 0;
+        if(pontuacaoJogador == pontuacaoMaquina)
+            return 0;
 
-    //     return pontuacaoJogador > pontuacaoMaquina ? 1 : 2; 
-    // }
+        return pontuacaoJogador > pontuacaoMaquina ? -1 : 1; 
+    }
 
     private void PreencheArvoreDePossibilidades(No no, bool[] jogadasPossiveis, bool jogador)
     {
@@ -189,48 +197,194 @@ public class Dots
         }
     }
 
-    // private int AvaliaMiniMax(No no, bool jogador)
-    // {
-    //     var ganhador = EncontrarGanhador(no);
+    private int AvaliaMiniMax(No no, bool jogador)
+    {
+        var ganhador = EncontrarGanhador(no);
 
-    //     if (ganhador != 2) // Se o jogo foi finalizado
-    //     {
-    //         if (ganhador == 1)
-    //         {
-    //             return 1;
-    //         }
-    //         else if (ganhador == -1)
-    //         {
-    //             return -1;
-    //         }
-    //         else
-    //         {
-    //             return 0;
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (jogador)
-    //         {
-    //             for (int i = 0; i < no.Filhos.Count; i++)
-    //             {
-    //                 var resultado = AvaliaMiniMax(no.Filhos[i], !jogador);
-    //                 if (resultado < no.ValorMinMax)
-    //                     no.ValorMinMax = resultado;
-    //             }
-    //             return no.ValorMinMax;
-    //         }
-    //         else if (!jogador)
-    //         {
-    //             for (int i = 0; i < no.Filhos.Count; i++)
-    //             {
-    //                 var resultado = AvaliaMiniMax(no.Filhos[i], !jogador);
-    //                 if (resultado > no.ValorMinMax)
-    //                     no.ValorMinMax = resultado;
-    //             }
-    //             return no.ValorMinMax;
-    //         }
-    //     }
-    //     return 0;
-    // }
+        if (ganhador != 2) // Se o jogo foi finalizado
+        {
+            if (ganhador == 1)
+            {
+                return 1;
+            }
+            else if (ganhador == -1)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            if (jogador)
+            {
+                for (int i = 0; i < no.Filhos.Count; i++)
+                {
+                    var resultado = AvaliaMiniMax(no.Filhos[i], !jogador);
+                    if (resultado < no.ValorMinMax)
+                        no.ValorMinMax = resultado;
+                }
+                return no.ValorMinMax;
+            }
+            else if (!jogador)
+            {
+                for (int i = 0; i < no.Filhos.Count; i++)
+                {
+                    var resultado = AvaliaMiniMax(no.Filhos[i], !jogador);
+                    if (resultado > no.ValorMinMax)
+                        no.ValorMinMax = resultado;
+                }
+                return no.ValorMinMax;
+            }
+        }
+        return 0;
+    }
+
+    private int Jogar(No no, bool jogador)
+    {
+        //Atualiza o tabuleiro e imprime
+        ExibirTabuleiro();
+
+        var ganhador = EncontrarGanhador(no);
+        if (ganhador != 2)
+        {
+            System.Console.WriteLine("Ganhador: "+ ganhador);                
+        } 
+        else
+        {
+            if (!jogador) //Maquina
+            {    
+                int melhorPontuacao = Int32.MinValue;
+                int indiceMelhorFilho = 0;
+                for (int i = 0; i < no.Filhos.Count; i++)
+                {
+                    if (no.Filhos[i].ValorMinMax > melhorPontuacao)
+                    {
+                        melhorPontuacao = no.Filhos[i].ValorMinMax;
+                        indiceMelhorFilho = i;
+                    }
+                }
+
+                var coordenadaDaJogada = (0, 0);
+
+                for(int i = 0; i < 5; i++)
+                {
+                    for(int j = 0; j < 5; j++)
+                    {
+                        if(no.Filhos[indiceMelhorFilho].Tabuleiro[i, j] != TabuleiroOficial[i, j])
+                        {
+                            coordenadaDaJogada = (i, j);
+                        }
+                    }
+                }
+
+                AtualizarTabuleiro(no.Filhos[indiceMelhorFilho].Tabuleiro);
+
+                //Exibe jogada
+                Thread.Sleep(2000);
+
+
+                if(no.Filhos[indiceMelhorFilho].VerificarPontuacao(coordenadaDaJogada, jogador) == true)
+                {
+                    Jogar(no.Filhos[indiceMelhorFilho], jogador);
+                }
+                else
+                {
+                    Jogar(no.Filhos[indiceMelhorFilho], !jogador);
+                }
+            }
+            else
+            {
+                int indiceFilhoCorreto = 0;
+
+                Console.WriteLine("Onde você quer jogar(número):");
+                string numero = Console.ReadLine() ?? "";
+
+                // Traduz o numero da tela para posição (x, y) da matriz
+                var coordenada = MapearPosicao(int.Parse(numero));
+
+                while(no.Tabuleiro[coordenada.linha, coordenada.coluna] != ' ')
+                { 
+                    if(no.Tabuleiro[coordenada.linha, coordenada.coluna] == 'x')
+                        Console.WriteLine("Outro jogador já fez essa jogada!");
+
+                    Console.WriteLine("Digite um valor válido:");
+                    numero = Console.ReadLine() ?? "";
+                    coordenada = MapearPosicao(int.Parse(numero));
+                }
+
+                TabuleiroOficial[coordenada.linha, coordenada.coluna] = 'x';
+
+                foreach (var (filho, index) in no.Filhos.LoopIndex())
+                {
+                    if (ComparaTabuleiro(filho.Tabuleiro))
+                    {
+                        indiceFilhoCorreto = index;
+                    }
+                }
+                Jogar(no.Filhos[indiceFilhoCorreto], !jogador);
+            }
+        }
+        return 0;
+    }
+
+    private void AtualizarTabuleiro(char[,] tabuleiroNovo)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                TabuleiroOficial[i, j] = tabuleiroNovo[i,j];
+            }
+        }
+    }
+
+    public bool ComparaTabuleiro(char[,] jogoDoNo)
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            for(int j = 0; j < 5; j++)
+            {
+                if(jogoDoNo[i, j] != TabuleiroOficial[i, j])
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void ExibirTabuleiro()
+    {
+        Console.WriteLine("Exibindo tabuleiro...\n");
+
+        for (int i = 0; i < TamanhoDoTabuleiro; i++)
+        {
+            for (int j = 0; j < TamanhoDoTabuleiro; j++)
+            {
+                Console.Write(" " + TabuleiroOficial[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+
+    public (int linha, int coluna) MapearPosicao(int posicao)
+        => posicao switch {
+            1 => (0, 1),
+            2 => (0, 3),
+            3 => (1, 0),
+            4 => (1, 2),
+            5 => (1, 4),
+            6 => (2, 1),
+            7 => (2, 3),
+            8 => (3, 0),
+            9 => (3, 2),
+            10 => (3, 4),
+            11 => (4, 1),
+            12 => (4, 3),
+            _ => default,
+        };
 }
